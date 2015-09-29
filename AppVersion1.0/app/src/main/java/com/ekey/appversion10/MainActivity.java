dd4d52885b80dec6cad69b049f546b97db1d7c28
Sharkalgud <sarkalgud@gmail.com>
2015-09-28 21:48:04 -0500

    added basic button

--- AppVersion1.0/app/src/main/java/com/ekey/appversion10/MainActivity.java
+++ AppVersion1.0/app/src/main/java/com/ekey/appversion10/MainActivity.java
@@ -4,13 +4,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
+    boolean lock = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+                if (lock) {
+                    button.setText("Lock door");
+                    lock = false;
+                } else {
+                    button.setText("Unlock door");
+                    lock = true;
+                }
+            }
+        });
     }
 
     @Override
