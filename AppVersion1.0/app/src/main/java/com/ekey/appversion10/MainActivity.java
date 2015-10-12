1708ca1bf91d3b578af0851d504699dc60f443d8
Sharkalgud <sarkalgud@gmail.com>
2015-10-12 00:23:03 -0500

    added bluetooth

--- AppVersion1.0/app/src/main/java/com/ekey/appversion10/MainActivity.java
+++ AppVersion1.0/app/src/main/java/com/ekey/appversion10/MainActivity.java
@@ -1,31 +1,65 @@
 package com.ekey.appversion10;
 
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.security.Security;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_ENABLE_BT = 3;
+    private static final int REQUEST_CONNECT_DEVICE_SECURE = 1;
+    private static final int REQUEST_CONNECT_DEVICE_INSECURE = 2;
     Button button;
+    Button button2;
+    BluetoothAdapter bluetoothAdapter;
+    BluetoothChatService chatService = null;
     boolean lock = true;
+    private StringBuffer stringBuffer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = (Button)findViewById(R.id.button);
-        button.setOnClickListener( new View.OnClickListener(){
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        button = (Button) findViewById(R.id.button);
+        button2 = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 if (lock) {
                     button.setText("Lock door");
                     lock = false;
+                    sendMessage("open");
                 } else {
                     button.setText("Unlock door");
                     lock = true;
+                    sendMessage("close");
+                }
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!bluetoothAdapter.isEnabled()){
+                    Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+                }
+                if(chatService == null){
+                    setupChat();
                 }
             }
         });
@@ -51,5 +85,123 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setupChat(){
+        // Initialize the BluetoothChatService to perform bluetooth connections
+        chatService = new BluetoothChatService(mHandler);
+        stringBuffer = new StringBuffer("");
+    }
+    private void sendMessage(String message) {
+        // Check that we're actually connected before trying anything
+        if (chatService.getState() != BluetoothChatService.STATE_CONNECTED) {
+            //Toast.makeText(getActivity(), R.string.not_connected, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Check that there's actually something to send
+        if (message.length() > 0) {
+            // Get the message bytes and tell the BluetoothChatService to write
+            byte[] send = message.getBytes();
+            chatService.write(send);
+
+            // Reset out string buffer to zero and clear the edit text field
+            stringBuffer.setLength(0);
+        }
+    }
+
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            //FragmentActivity activity = getActivity();
+            switch (msg.what) {
+                case Constants.MESSAGE_STATE_CHANGE:
+                    switch (msg.arg1) {
+                        case BluetoothChatService.STATE_CONNECTED:
+                            //setStatus(getString(R.string.title_connected_to, mConnectedDeviceName));
+                            //mConversationArrayAdapter.clear();
+                            break;
+                        case BluetoothChatService.STATE_CONNECTING:
+                            //setStatus(R.string.title_connecting);
+                            break;
+                        case BluetoothChatService.STATE_LISTEN:
+                        case BluetoothChatService.STATE_NONE:
+                            //setStatus(R.string.title_not_connected);
+                            break;
+                    }
+                    break;
+                case Constants.MESSAGE_WRITE:
+                    byte[] writeBuf = (byte[]) msg.obj;
+                    // construct a string from the buffer
+                    String writeMessage = new String(writeBuf);
+                    //mConversationArrayAdapter.add("Me:  " + writeMessage);
+                    break;
+                case Constants.MESSAGE_READ:
+                    byte[] readBuf = (byte[]) msg.obj;
+                    // construct a string from the valid bytes in the buffer
+                    String readMessage = new String(readBuf, 0, msg.arg1);
+                    //mConversationArrayAdapter.add(mConnectedDeviceName + ":  " + readMessage);
+                    break;
+                case Constants.MESSAGE_DEVICE_NAME:
+                    // save the connected device's name
+                    //mConnectedDeviceName = msg.getData().getString(Constants.DEVICE_NAME);
+                    /*if (null != activity) {
+                        Toast.makeText(activity, "Connected to "
+                                + mConnectedDeviceName, Toast.LENGTH_SHORT).show();
+                    }*/
+                    break;
+                case Constants.MESSAGE_TOAST:
+                    /*if (null != activity) {
+                        Toast.makeText(activity, msg.getData().getString(Constants.TOAST),
+                                Toast.LENGTH_SHORT).show();
+                    }*/
+                    break;
+            }
+        }
+    };
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_CONNECT_DEVICE_SECURE:
+                // When DeviceListActivity returns with a device to connect
+                if (resultCode == Activity.RESULT_OK) {
+                    connectDevice(data, true);
+                }
+                break;
+            case REQUEST_CONNECT_DEVICE_INSECURE:
+                // When DeviceListActivity returns with a device to connect
+                if (resultCode == Activity.RESULT_OK) {
+                    connectDevice(data, false);
+                }
+                break;
+            case REQUEST_ENABLE_BT:
+                // When the request to enable Bluetooth returns
+                if (resultCode == Activity.RESULT_OK) {
+                    // Bluetooth is now enabled, so set up a chat session
+                    setupChat();
+                } else {
+                    // User did not enable Bluetooth or an error occurred
+                    //Log.d(TAG, "BT not enabled");
+                    //Toast.makeText(getActivity(), R.string.bt_not_enabled_leaving,
+                            //Toast.LENGTH_SHORT).show();
+                    //getActivity().finish();
+                }
+        }
+    }
+
+    //**
+     //* Establish connection with other divice
+    // *
+    // * @param data   An {@link Intent} with {@link DeviceListActivity#EXTRA_DEVICE_ADDRESS} extra.
+    // * @param secure Socket Security type - Secure (true) , Insecure (false)
+    // *
+    private void connectDevice(Intent data, boolean secure) {
+        // Get the device MAC address
+       // String address = data.getExtras(.getString(DeviceListActivity.EXTRA_DEVICE_ADDRESS);
+        // Get the BluetoothDevice object
+        String address = "dad8bf14-b6c3-45fa-b9a7-94c1fde2e7c6";
+        BluetoothDevice device = bluetoothAdapter.getRemoteDevice(address);
+        // Attempt to connect to the device
+        chatService.connect(device, secure);
     }
 }
